0c6b99fb1e72ee32bb042eb76994a8c803621058
DarkSnowJR <amirkiankiani@icloud.com>
2021-07-21 18:05:47 +0430

    income app

--- income/views.py
+++ income/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import ExpenseSerializer
-from .models import Expense
+from .serializers import IncomeSerializer
+from .models import Income
 from rest_framework import permissions
 from .permissions import IsOwner
 
 
 # Create your views here.
-class ExpenseListAPIView(ListCreateAPIView):
-    serializer_class = ExpenseSerializer
-    queryset = Expense.objects.all()
+class IncomeListAPIView(ListCreateAPIView):
+    serializer_class = IncomeSerializer
+    queryset = Income.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
@@ -19,10 +19,10 @@ class ExpenseListAPIView(ListCreateAPIView):
         return self.queryset.filter(owner=self.request.user)
 
 
-class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
-    serializer_class = ExpenseSerializer
+class IncomeDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = IncomeSerializer
     permission_classes = (permissions.IsAuthenticated, IsOwner,)
-    queryset = Expense.objects.all()
+    queryset = Income.objects.all()
     lookup_field = "id"
 
     def get_queryset(self):
